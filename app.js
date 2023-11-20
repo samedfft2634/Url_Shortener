3f749674dd20b3879999d235c30abed9f27973b1
samedfft2634 <samedfft@gmail.com>
2023-11-20 18:31:00 +0300

    javascript fixed

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ const clipBoard = document.getElementById("clipBoard");
 const goLink = document.getElementById("goLink");
 const goSource = document.getElementById("goSource");
 const content = document.getElementById("content");
+const results = document.getElementById("results");
 
 const getData = async () => {
 	const response = await fetch(
 		`https://tinyurl.com/api-create.php?url=` +
 			encodeURIComponent(inputUrl.value)
 	);
-	if (response.ok) {
+	if (response.status == 200) {
 		const data = await response.text();
 		shortUrl.value = data;
 	} else {
-		inputUrl.value = ""
+		inputUrl.value = "";
 		content.innerHTML = `<h1 style="text-align:center; color: #fff">Invalid URL ‼</h1>`;
 		const body = document.querySelector("body");
 		body.style.flexDirection = "column";
@@ -27,21 +28,31 @@ const getData = async () => {
 		}, 2000);
 	}
 };
-
+inputUrl.addEventListener("focus",()=> inputUrl.value = "")
 shortBtn.addEventListener("click", getData);
 removeBtn.addEventListener("click", () => {
 	shortUrl.value = "";
 	inputUrl.value = "";
+	goSource.href = "#";
+	goSource.target = "";
 });
 goLink.addEventListener("click", () => {});
 
 const copyClip = () => {
-	const url = document.getElementById("shortUrl");
-	url.select();
-	document.execCommand("copy");
-	shortUrl.value = "Copied ✅";
-	shortUrl.style.color = "green";
-	shortUrl.style.textAlign = "end";
+	if (shortUrl.value.trim() ) {
+		const url = document.getElementById("shortUrl");
+		url.select();
+		document.execCommand("copy");
+		results.textContent = "Copied ✅";
+		results.style.color = "green";
+		results.style.textAlign = "end";
+		timeOut();
+	} else {
+		results.textContent = "No  link 👻";
+		results.style.color = "#fff";
+		results.style.textAlign = "end";
+		timeOut();
+	}
 };
 clipBoard.addEventListener("click", copyClip);
 
@@ -54,7 +65,36 @@ goSource.addEventListener("click", () => {
 		goSource.href = shortUrl.value;
 		goSource.target = "_blank";
 	} else {
-		alert("Bulunamadi.");
+		if(inputUrl.value && !shortUrl.value){
+			Swal.fire({
+				position: "center",
+				icon: "info",
+				title: "Short it before go link!",
+				showConfirmButton: false,
+				timer: 1000,
+			});
+		}else{
+			swal()
+		}
 	}
 });
-//https://www.youtube.com/watch?v=foa1jeLLXI0
+
+const swal = () => {
+	Swal.fire({
+		position: "center",
+		icon: "question",
+		title: "No link found!",
+		showConfirmButton: false,
+		timer: 1000,
+	});
+};
+
+const timeOut = () => {
+	setTimeout(() => {
+		results.textContent = "";
+		results.style.color = "#000";
+		results.style.textAlign = "";
+	}, 1000);
+};
+
+// Try link for debugging : //https://www.youtube.com/watch?v=foa1jeLLXI0
